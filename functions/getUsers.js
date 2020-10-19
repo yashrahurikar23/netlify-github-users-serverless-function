4506a456529bca4633f680ce1b60c351bd1a807f
Yash Rahurikar <2yashrahurikar@gmail.com>
2020-10-19 07:48:41 +0530

    UI implementation to render the fetched users

--- functions/getUsers.js
+++ functions/getUsers.js
@@ -11,6 +11,10 @@ exports.handler = function(event, context, callback) {
   const send = body => {
     callback(null, {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      },
       body: JSON.stringify(body)
     });
   }
